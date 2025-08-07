b0837ad3a15484240996b21233fddbab9c415d34
josivaldocruz <josivaldonet@gmail.com>
2025-08-07 23:51:17 +0000

    Atualização

---
+++ Aula 02/Exercício.py
@@ -0,0 +1,29 @@
+#Exercicio 03
+print("Nome: João" , "\nIdade: 20 anos" , "\nCurso: Python Basico")
+
+#Exercicio 04
+Nome = "João"
+Idade = "20 anos"
+Curso = "Python Basico"
+print("Eu sou",Nome,"Tenho",Idade,"e faço parte do curso de",Curso)
+
+#Exercicio 05
+print("#####\n#   #\n#   #\n#####")
+
+#Exercicio 06
+
+num1 = 10
+num2 = 3
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)
+print(num1**num2)
+
+#Exercicio 07
+
+
+#Exercicio 08
