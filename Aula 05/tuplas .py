8178d94c312863fd54c3cfcf7a4a10211ba43b70
josivaldocruz <josivaldonet@gmail.com>
2025-08-19 23:29:03 +0000

    a

---
+++ Aula 05/tuplas .py
@@ -0,0 +1,23 @@
+#tuple  não consegue fazer edição, tipo de lista não editavel e mais leve
+# virgula ou parentes
+# tuplas1 = 1,
+# tuplas1 = ()
+
+# print(type(tuplas1))
+
+# tuplas2 = (1,2,3,4)
+# for num in tuplas2:
+# #print(num+=1)
+
+# tuplas3 = ("fred","jose","freD")
+
+# tuplas3.index
+
+# print(tuplas3.count("fred"))
+teste = {"OI"}
+teste2 = {}
+
+print(type(teste))
+print(type(teste2))
+
+teste.add
