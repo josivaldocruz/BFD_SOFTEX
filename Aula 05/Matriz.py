183918c0e07c16a65d20225d9eb3e806339655cf
josivaldocruz <josivaldonet@gmail.com>
2025-08-19 23:03:01 +0000

    a

--- Aula 05/Matriz.py
+++ Aula 05/Matriz.py
@@ -5,26 +5,34 @@
 
 # # print(matriz)
 
-# matriz2 = [[1,2,3],[4,5,6],[7,8,9],
-#            [1,2,3],[4,5,6],[7,8,9],
-#            [1,2,3],[4,5,6],[7,8,9]]
+# matriz2 = [[1,2,3],[4,5,6],[7,8,9]]
+
+# for linha in matriz2:
+#     for elemento in linha:
+#         print(f"{elemento:2}",  end=" ")
+# print()
 
 
-# print(matriz2[1],[1],[1])
 
-lista1 = []
+
+# lista1 = []
 
 # for i in range(10):
 #     lista1.append(2)
 
 # print(lista1)
 
-for i in range(10):
-    lista1.append(i*2)
+# for i in range(10):
+#     lista1.append(i*2)
 
-print(lista1)
+# print(lista1)
 
 #lista3 = (for i in range(10):lista1.append(2))
 
 #lista4 = (for linha in range(3) for coluna in range(3))
+
+
+
+
+
 
