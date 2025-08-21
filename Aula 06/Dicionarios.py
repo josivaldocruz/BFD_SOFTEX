357525b3e1bdf9e535291c8596047510724a0667
josivaldocruz <josivaldonet@gmail.com>
2025-08-21 23:06:59 +0000

    a

---
+++ Aula 06/Dicionarios.py
@@ -0,0 +1,21 @@
+#Dicionarios
+
+dicionario = {
+    "nome": "jo",
+    "Idade": 37,
+    "Filmes_favorito": "dark"}
+
+
+
+print(type(dicionario))
+
+#print(dicionario)
+
+for item in dicionario.items():
+
+    print(dicionario)
+
+
+# instalar bm Python -m venv .vemvenv
+
+# online 
