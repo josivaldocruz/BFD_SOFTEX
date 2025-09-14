f67075b0542cb828bd33efb9ad1b487c99529eef
josivaldocruz <josivaldonet@gmail.com>
2025-09-14 22:02:12 +0000

    atualização

--- EXERCICIO_CLASSES_OBJETO_02/Exercicio_01.py
+++ EXERCICIO_CLASSES_OBJETO_02/Exercicio_01.py
@@ -2,5 +2,39 @@
 # Crie um método setter e um getter para acessar e modificar essa atributo,
 # seguindo uma regra lógica (Ex: saldo não pode ser negativo)
 
+class ContaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.__saldo = saldo
 
+        # Getters
+    def get_saldo(self):
+        return self.__saldo
+    
+        # Setters
+    def set_saldo(self, valor):
+        if valor < 0:
+            print("Saldo não pode ser negativo")
+        else:
+            self.__saldo = valor
+
+    def depositar(self, valor):
+        self.__saldo += valor
+        print(f"\nOlá {self.titular}, Seu novo saldo é: {self.__saldo}.")
+
+    def sacar(self, valor):
+        if valor > self.__saldo:
+            print(f"\nOlá {self.titular}, seu saldo é insuficiente para saque!, saldo atual é: {self.__saldo}.")
+        else:
+            self.__saldo -= valor
+        return print(f"\nOlá {self.titular}, seu saldo atual é: {self.__saldo}.")
+
+
+operacao = ContaBancaria("João",80)
+
+print(operacao.get_saldo())
+
+operacao.depositar(50)
+ 
+operacao.sacar(20)
 
--- EXERCICIO_CLASSES_OBJETO_02/Exercicio_02.py
+++ EXERCICIO_CLASSES_OBJETO_02/Exercicio_02.py
@@ -1,6 +1,44 @@
 # Crie uma classe, Pessoa, que tenha os atributos: nome,
-# data de nascimento, cpf, identidade. Deixe os atributos 
+# data de nascimento, cpf, identidade. Deixe os atributos
 # Cpf e identidade como privado e monte os métodos setters e getters
 # para acessar e editar os valores
 
 
+class Pessoa:
+    def __init__(self, nome, data_de_nascimento, cpf, identidade):
+        self. nome = nome
+        self.data_nascimento = data_de_nascimento
+        self.__cpf = cpf
+        self.__identidade = identidade
+
+    # getters
+    def get_cpf(self):
+        return self.__cpf
+
+    def get_identidade(self):
+        return self.__identidade
+    # setters
+
+    def set_cpf(self, valor):
+        if len(str(valor)) == 11:
+            self.__cpf = valor
+        else:
+            print("CPF deve ter 11 digitos")
+
+    def set_identidade(self, valor2):
+        if len(str(valor2)) == 6:
+            self.__identidade = valor2
+        else:
+            print("Identidade deve ter 6 digitos")
+
+
+pessoa1 = Pessoa("Josivaldo", "03/09/1987", 22222222222, 111111)
+
+print(pessoa1.get_cpf())
+print(pessoa1.get_identidade())
+
+pessoa1.set_cpf(33333333333)
+pessoa1.set_identidade(123456)
+
+print(pessoa1.get_cpf())
+print(pessoa1.get_identidade())
