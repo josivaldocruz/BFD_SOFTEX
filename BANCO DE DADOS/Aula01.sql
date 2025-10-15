7cde5e555cfb036c04579fbabb0a76a8d037156b
josivaldocruz <josivaldonet@gmail.com>
2025-10-15 23:56:14 +0000

    sql

--- BANCO DE DADOS/Aula01.sql
+++ BANCO DE DADOS/Aula01.sql
@@ -1,1 +1,28 @@
-select 22
+/*
+Aula SQL Turma 36
+Usando SQlite
+*/
+-- comando para listar tabela
+--.tables
+
+-- ver o que tem na tabela
+--.schema Aluno
+--.schema Turma
+
+-- query para acesar od dados da tabela
+--Select * from aluno;
+
+--Select nome from aluno;
+
+--pegar varias colunas
+--Select nome, id, nota1 from aluno;
+
+
+-- Select nome, nota1, nota2 from aluno
+-- where nota1 >= 7 and nota2 >=7
+-- ;
+
+-- Select nome, nota1, nota2 from aluno
+
+-- order by asc ou desc
+
